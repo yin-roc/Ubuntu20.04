ed522ac9a98b75bf1a87a04d07c5f6e2de0c20e9
yin-roc <yin15295770968@163.com>
2023-10-16 22:20:07 +0800

    复习：基本订阅者和发布者
    tips：1、中文乱码问题；2、发布者延迟发布；3、控制发布者发布频率；4、字符串拼接：std::stringstream

---
+++ ROS_demo/demo01_ws/build/plumbing_pub_sub/CMakeFiles/demo01_pub_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/devel/lib/plumbing_pub_sub/demo01_pub_c"
+  "/home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/devel/lib/plumbing_pub_sub/demo01_pub_c.pdb"
+  "CMakeFiles/demo01_pub_c.dir/src/demo01_pub_c.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo01_pub_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ROS_demo/demo01_ws/build/plumbing_pub_sub/CMakeFiles/demo02_sub_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/devel/lib/plumbing_pub_sub/demo02_sub_c"
+  "/home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/devel/lib/plumbing_pub_sub/demo02_sub_c.pdb"
+  "CMakeFiles/demo02_sub_c.dir/src/demo02_sub_c.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo02_sub_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
