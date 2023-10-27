aa65b57419f0c0d9c9672814efac42d94f332de9
yin-roc <yin15295770968@163.com>
2023-10-27 17:23:47 +0800

    命名空间与节点名称重映射

---
+++ ROS_demo/demo01_ws/build/ten_rename01_ns_name/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/src/ten_rename01_ns_name
+# Build directory: /home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo01_ws/build/ten_rename01_ns_name
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
