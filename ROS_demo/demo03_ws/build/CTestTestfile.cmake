b825b698c52cf84bba6462fff90d1707f93d21f6
yin-roc <yin15295770968@163.com>
2023-11-14 20:45:35 +0800

    URDF集成rviz

---
+++ ROS_demo/demo03_ws/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo03_ws/src
+# Build directory: /home/yin-roc/1_Code/Ubuntu20.04/ROS_demo/demo03_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arbotix_ros/arbotix")
+subdirs("arbotix_ros/arbotix_controllers")
+subdirs("arbotix_ros/arbotix_firmware")
+subdirs("arbotix_ros/arbotix_python")
+subdirs("arbotix_ros/arbotix_sensors")
+subdirs("arbotix_ros/arbotix_msgs")
+subdirs("one_urdf01_rviz")
