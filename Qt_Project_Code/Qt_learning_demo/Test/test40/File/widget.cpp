606aff3715abe0679333a73daa9bedd45b80a6ae
yin-roc <yin15295770968@163.com>
2024-01-25 13:12:38 +0800

    视频43：文件读写

---
+++ Qt_Project_Code/Qt_learning_demo/Test/test40/File/widget.cpp
@@ -0,0 +1,40 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QFileDialog>
+#include <QFile>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton, &QPushButton::clicked, this, [&](){
+        QString filename = QFileDialog::getOpenFileName(this, "打开文件", "/home/yin-roc/1_Code/Ubuntu20.04/Qt_Project_Code/Qt_learning_demo/Test/test40/File");
+        ui->lineEdit->setText(filename);
+        QFile file(filename); // 将QFile对象关联到指定文件
+
+        // 读
+        file.open(QIODevice::ReadOnly);
+//        QByteArray array = file.readAll(); // 读取全部内容
+        QByteArray array;
+        while(!file.atEnd())
+        {
+            array += file.readLine(); // 一行一行的读取
+        }
+        ui->textEdit->setText(array); // 此处发生了隐式转换
+        file.close();
+
+        // 写
+        file.open(QIODevice::Append); // 单纯写入会覆盖原有内容，因此选择在原文末尾追加
+        file.write("I love you, Rick!");
+        file.close(); // 隐式转换
+
+    });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
